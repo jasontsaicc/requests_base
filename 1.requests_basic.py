965617e70958a32dde595f1e5bdb555cf7f6ed85
jasontsai <jasontsaicc@gmail.com>
2021-02-06 00:25:11 +0800

    requests_響應對象的其它常用屬性或方法

--- 1.requests_basic.py
+++ 1.requests_basic.py
@@ -14,3 +14,19 @@ response.encoding = 'utf8'
 # response.content 是存儲的bytes類型的響應源碼,可以進行decode操作
 print(response.content.decode())
 
+# 常見的響應對象參數和方法
+# 響應url
+print(response.url)
+
+# 狀態碼
+print(response.status_code)
+
+# 響應對應的請求頭
+print(response.request.headers)
+
+# 響應頭
+print(response.headers)
+
+# 響應設置的cookie
+print(response.cookies)
+
